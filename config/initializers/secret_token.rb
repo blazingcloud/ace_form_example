3a923afabda291b2d9f7458dcd0ebd84824952e2
Sarah Allen <sarah@ultrasaurus.com>
2011-09-16 22:27:06 -0700

    rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AceForm::Application.config.secret_token = 'c1b3241e1cdf83ed5b2e3e9bbb390239222a9c603176883e2a9795e3308fb8f660271bcda47113176e72c8fe0b0b4b479a9dd6e304105fbe239ff13d77201299'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AceForm::Application.config.session_store :cookie_store, key: '_ace_form_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AceForm::Application.config.session_store :active_record_store
